06be1e8e40502d866aa5105cad70a00df988aef4
Richard Pitts <Richard.pitts@resideo.com>
2022-10-01 20:59:05 -0500

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import {ThemeProvider, StyledEngineProvider} from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <StyledEngineProvider injectFirst>
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
       <App />
     </ThemeProvider>
-  </StyledEngineProvider>,
-    document.getElementById('root'),
+  </StyledEngineProvider>
 );
 
 
